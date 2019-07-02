606167bfd413ea8feade2ff95ffd1d413f604744
yangqingren <564008993@qq.com>
2019-07-02 14:36:28 +0800

    0.1.1

--- LEGOPopover/Classes/LGAlertGlobalParameter.m
+++ LEGOPopover/Classes/LGAlertGlobalParameter.m
@@ -25,10 +25,34 @@ isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bo
     configure.cancelColor = [UIColor colorWithWhite:1 alpha:0.5];
     configure.completColor = [UIColor colorWithRed:171 /255.0 green:142 /255.0 blue:78 /255.0 alpha:1];
     
-    configure.titleFont = [UIFont systemFontOfSize:19 weight:UIFontWeightSemibold];
-    configure.detailFont = [UIFont systemFontOfSize:15 weight:UIFontWeightSemibold];
-    configure.cancelFont = [UIFont systemFontOfSize:17 weight:UIFontWeightRegular];
-    configure.completFont = [UIFont systemFontOfSize:17 weight:UIFontWeightRegular];
+    if (@available(iOS 8.2, *)) {
+        configure.titleFont = [UIFont systemFontOfSize:19 weight:UIFontWeightSemibold];
+    } else {
+        configure.titleFont = [UIFont systemFontOfSize:19];
+        // Fallback on earlier versions
+    }
+    
+    if (@available(iOS 8.2, *)) {
+        configure.detailFont = [UIFont systemFontOfSize:15 weight:UIFontWeightSemibold];
+    } else {
+        configure.detailFont = [UIFont systemFontOfSize:15];
+        // Fallback on earlier versions
+    }
+    
+    if (@available(iOS 8.2, *)) {
+        configure.cancelFont = [UIFont systemFontOfSize:17 weight:UIFontWeightRegular];
+    } else {
+        configure.cancelFont = [UIFont systemFontOfSize:17];
+        // Fallback on earlier versions
+    }
+    
+    if (@available(iOS 8.2, *)) {
+        configure.completFont = [UIFont systemFontOfSize:17 weight:UIFontWeightRegular];
+    } else {
+        configure.completFont = [UIFont systemFontOfSize:17];
+        // Fallback on earlier versions
+    }
+    
     
     configure.viewHeight = 245 + (IS_IPHONE_X ? 34 : 0);
     !make ? :make(configure);
